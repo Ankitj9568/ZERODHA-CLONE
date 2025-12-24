359988ea560ec23902f66646627694bb3f40bdca
Ankitj9568 <ankitjha9568@gmail.com>
2025-12-24 20:47:42 +0530

    update3

---
+++ src/landing_page/signup/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+function SignUp() {
+    return ( 
+        <>
+             <Navbar/>
+             <Footer/>
+        </>
+     );
+}
+
+export default SignUp;
