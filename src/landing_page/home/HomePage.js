d3fd52f9340cce6512cc2f0f6236968003c42da3
Ankitj9568 <ankitjha9568@gmail.com>
2026-01-17 11:38:11 +0530

    Home2

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -3,26 +3,24 @@ import Hero from './Hero';
 import Stats from './Stats';
 import Pricing from './Pricing';
 import Education from './Education';
-import Awards from './Awards';
 
 import OpenAccount from '../OpenAccount';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 
 function HomePage() {
-    return ( 
+    return (
         //Order should be maintained as per the UI/UX design
         <>
-            <Navbar/>
+            <Navbar />
             <Hero />
-            <Awards />
             <Stats />
             <Pricing />
             <Education />
-            <OpenAccount/>
-            <Footer/>
+            <OpenAccount />
+            <Footer />
         </>
-     );
+    );
 }
 
 export default HomePage;
